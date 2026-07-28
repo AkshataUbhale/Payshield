c1a1484563b849f01dfbe383537ec1c350af3ea1
Akshataubhale <akshatagithub@gmail.com>
2026-07-28 22:30:17 +0530

    Remove old repo files

--- Frontend/src/hooks/useWallet.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { WalletContext } from "../context/WalletContext";
-
-export function useWallet() {
-  const ctx = useContext(WalletContext);
-  if (!ctx) throw new Error("useWallet must be used inside WalletProvider");
-  return ctx;
-}
